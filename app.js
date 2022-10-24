6be7710b296832d4225ea0a8c71bdea2238d9f1e
Jaytee-Thomas <jt.aws.cloud@protonmail.com>
2022-10-24 10:47:58 -0500

    final commit JS Selection statement exercise

---
+++ app.js
@@ -0,0 +1,122 @@
+//JS Selection Statements
+//Exercise 1
+
+let favoriteNumber = Math.random() * 100;
+
+let userNumber = window.prompt("What is your favorite number?");
+
+if (userNumber < favoriteNumber) {
+  window.alert("Your number is too low!");
+} else if (userNumber > favoriteNumber) {
+  window.alert("Your number is too high!");
+} else {
+  window.alert("Your number is correct!");
+}
+
+//Exercise 2
+
+let birthMonth = window.prompt("What is your birth month?");
+
+switch (birthMonth) {
+  case "January || january":
+    console.log("You were born in the winter!");
+    break;
+  case "February || february":
+    console.log("You were born in the winter!");
+    break;
+  case "March || march":
+    console.log("You were born in the spring!");
+    break;
+  case "April || april":
+    console.log("You were born in the spring!");
+    break;
+  case "May || may":
+    console.log("You were born in the spring!");
+    break;
+  case "June || june":
+    console.log("You were born in the summer!");
+    break;
+  case "July || july":
+    console.log("You were born in the summer!");
+    break;
+  case "August || august":
+    console.log("You were born in the summer!");
+    break;
+  case "September || september":
+    console.log("You were born in the fall!");
+    break;
+  case "October || october":
+    console.log("You were born in the fall!");
+    break;
+  case "November || november":
+    console.log("You were born in the fall!");
+    break;
+  case "December || december":
+    console.log("You were born in the winter!");
+    break;
+  default:
+    console.log("You did not enter a valid month!");
+}
+
+//Exercise 3
+
+let typeId = "01";
+let colorId = "PU";
+let sizeId = "L";
+
+let type = "";
+let color = "";
+let size = "";
+
+switch (typeId) {
+  case "01":
+    type = "Wife Beater";
+    break;
+  case "02":
+    type = "T-Shirt";
+    break;
+  case "03":
+    type = "Long Sleeve";
+    break;
+  case "04":
+    type = "Sweat Shirt";
+    break;
+  case "05":
+    type = "Other";
+}
+
+switch (colorId) {
+  case "BK || bk":
+    color = "Black";
+    break;
+  case "BL || bl":
+    color = "Blue";
+    break;
+  case "RD || rd":
+    color = "Red";
+    break;
+  case "PR || pr":
+    color = "Purple";
+    break;
+  case "WH || wh":
+    color = "White";
+}
+
+switch (sizeId) {
+  case "S || s":
+    size = "Small";
+    break;
+  case "M || m":
+    size = "Medium";
+    break;
+  case "L || l":
+    size = "Large";
+    break;
+  case "XL || xl":
+    size = "Extra Large";
+    break;
+  default:
+    size = "One Size Fits All";
+}
+
+console.log(`Product: ${size} ${color} ${type}`);
